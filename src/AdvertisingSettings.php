96d0955582b00a96678a6890e9c08aaa119d681f
Leon Stafford <me@ljs.dev>
2021-01-26 12:03:20 +1000

    register post metas

--- src/AdvertisingSettings.php
+++ src/AdvertisingSettings.php
@@ -25,6 +25,11 @@ final class AdvertisingSettings
                 'enqueue_block_editor_assets',
                 [$this, 'enqueue_admin_scripts']
             );
+
+            add_action(
+                'init',
+                [$this, 'advertising_settings_register_meta']
+            );
         }
     }
 
@@ -35,6 +40,39 @@ final class AdvertisingSettings
             plugins_url( '../build/index.js', __FILE__ ),
             [ 'wp-plugins', 'wp-edit-post', 'wp-blocks', 'wp-element', 'wp-components', 'wp-editor' ],
             (string)filemtime( plugin_dir_path( __FILE__ ) . '/index.js' )   
+        );
+    }
+
+    public function advertising_settings_register_meta(): void
+    {
+        register_meta(
+            'post',
+            '_advertising_settings_advertisements_metafield',
+            [
+                'show_in_rest' => true,
+                'type' => 'boolean',
+                'single' => true,
+            ]
+        );
+
+        register_meta(
+            'post',
+            '_advertising_settings_commercial_content_type_metafield',
+            [
+                'show_in_rest' => true,
+                'type' => 'string',
+                'single' => true,
+            ]
+        );
+
+        register_meta(
+            'post',
+            '_advertising_settings_advertiser_name_metafield',
+            [
+                'show_in_rest' => true,
+                'type' => 'string',
+                'single' => true,
+            ]
         );
     }
 }
