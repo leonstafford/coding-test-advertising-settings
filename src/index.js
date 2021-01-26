e5b45917a594213657431cefcbfe5544ddef90fe
Leon Stafford <me@ljs.dev>
2021-01-26 19:23:29 +1000

    wip conditionally create controls

--- src/index.js
+++ src/index.js
@@ -3,82 +3,47 @@ import ControlCreate from './modules/control'
 const { __ } = wp.i18n
 const { registerPlugin } = wp.plugins
 const { PluginDocumentSettingPanel } = wp.editPost
-const { TextControl, TextareaControl } = wp.components
+const { TextControl, ToggleControl, RadioControl } = wp.components
 const { withSelect, withDispatch } = wp.data
 
-
-/**
- * Title
- */
-
-let TitleController = ControlCreate({
-  component: TextControl,
+let Advertisements = ControlCreate({
+  component: ToggleControl,
   name: 'advertising_settings_advertisements_metafield'
 })
 
-
-/**
- * Description
- */
-
-let Description = ControlCreate({
-  component: TextareaControl,
+let ContentType = ControlCreate({
+  component: RadioControl,
   name: 'advertising_settings_commercial_content_type_metafield'
 })
 
-
-/**
- * Intro
- */
-
-let Intro = ControlCreate({
-  component: TextareaControl,
+let AdvertiserName = ControlCreate({
+  component: TextControl,
   name: 'advertising_settings_advertiser_name_metafield'
 })
 
-
-/**
- * Source
- */
-
-let Source = ControlCreate({
-  component: TextControl,
-  name: 'source_meta'
-})
 
 
 /**
  * Register Plugin
  */
-
-registerPlugin('ag-title-panel', {
+registerPlugin('advertising-settings-panel', {
   render: () => {
     return (
       <div>
         <PluginDocumentSettingPanel
-          name='ag-title-desc-panel'
-          title={ __( 'SEO Fields', 'agsf' ) }
+          name='advertising-settings-panel'
+          title={ 'Advertising Settings' }
         >
-          <label>{ __( 'Title', 'agsf' ) }</label>
-          <TitleController/>
+          <label>{ 'Advertisements' }</label>
+          <Advertisements/>
 
-          <label>{ __( 'Description', 'agsf' ) }</label>
-          <Description/>
+          <label>{ 'Commercial Content Type' }</label>
+          <ContentType />
+
+          <label>{ 'Advertiser Name' }</label>
+          <AdvertiserName/>
         </PluginDocumentSettingPanel>
 
-        <PluginDocumentSettingPanel
-          name='ag-intro-panel'
-          title={ __( 'Introduction', 'agsf' ) }
-        >
-          <Intro/>
-        </PluginDocumentSettingPanel>
-
-        <PluginDocumentSettingPanel
-          name='ag-source-panel'
-          title={ __( 'Source', 'agsf' ) }
-        >
-          <Source/>
-        </PluginDocumentSettingPanel>
       </div>
     )
   },
--- src/modules/control.js
+++ src/modules/control.js
@@ -9,10 +9,50 @@ let ControlCreate = controlProps => {
   let Control = props => {
     let ControlComponent = controlProps.component
 
-    return <ControlComponent
-      value={props[controlName]}
-      onChange={(value) => props.onMetaFieldChange(value, controlName)}
-    />
+
+    console.log('creating control:');
+    console.log(controlProps.name);
+
+    // TODO: conditionally set attributes based on type
+
+    if (controlProps.name === 'advertising_settings_advertisements_metafield') {
+      return <ControlComponent
+        value={props[controlName]}
+        onChange={(value) => props.onMetaFieldChange(value, controlName)}
+      />
+
+      // return <ControlComponent
+      //   value={props[controlName]}
+      //   checked={ props[controlName] }
+      //   onChange={(value) => props.onMetaFieldChange(! props[controlName], controlName)}
+      // />
+
+    } else if (controlProps.name === 'advertising_settings_commercial_content_type_metafield') {
+      return <ControlComponent
+        value={props[controlName]}
+        onChange={(value) => props.onMetaFieldChange(value, controlName)}
+      />
+
+      // return <ControlComponent
+      //   value={props[controlName]}
+      //   selected={ props[controlName] }
+      //   options={ [
+      //     { label: 'None', value: 'none' },
+      //     { label: 'Sponsored content', value: 'sponsored_content' },
+      //     { label: 'Partnered content', value: 'partnered_content' },
+      //     { label: 'Brought to you by', value: 'brought_to_you_by' },
+      //   ] }
+      //   onChange={(value) => props.onMetaFieldChange(value, controlName)}
+      // />
+
+    } else if (controlProps.name === 'advertising_settings_advertiser_name_metafield') {
+
+      return <ControlComponent
+        value={props[controlName]}
+        onChange={(value) => props.onMetaFieldChange(value, controlName)}
+      />
+
+    }
   }
 
   Control = withSelect(
@@ -31,7 +71,16 @@ let ControlCreate = controlProps => {
       return {
         onMetaFieldChange: (value, controlName) => {
           console.log(`Meta field value ${value} for ${controlName}`);
-          dispatch('core/editor').editPost({ meta: { [controlNameMeta]: value } })
+
+          if (controlName === 'advertising_settings_advertisements_metafield') {
+            // dispatch('core/editor').editPost({ meta: { [controlNameMeta]: (value  == 'true')} })
+            dispatch('core/editor').editPost({ meta: { [controlNameMeta]: value} })
+
+          } else {
+            dispatch('core/editor').editPost({ meta: { [controlNameMeta]: value } })
+
+          }
+
         }
       }
     }
